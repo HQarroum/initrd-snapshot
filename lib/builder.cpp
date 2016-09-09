a514db5025a50cee027571ab4d4d0fa7fc8031a3
Halim Qarroum <hqm.post@gmail.com>
2016-09-09 17:40:31 +0200

    Add comments

--- lib/builder.cpp
+++ lib/builder.cpp
@@ -1,17 +1,32 @@
-#include <initrd/builder.hpp>
+#include <builder.hpp>
 
 namespace initrd
 {
+  /**
+   * \constructor
+   */
   builder_t::builder_t() {}
 
+  /**
+   * \brief Adds a new file to the image under
+   * construction.
+   */
   builder_t& builder_t::addFile(file_t file) {
 
   }
 
+  /**
+   * \brief Adds a list of files to the image
+   * under construction.
+   */
   builder_t& builder_t::addFiles(std::list<file_t> files) {
 
   }
 
+  /**
+   * \return a new image instance built using the
+   * previously given file instances.
+   */
   const image_t builder_t::build() {
 
   }
